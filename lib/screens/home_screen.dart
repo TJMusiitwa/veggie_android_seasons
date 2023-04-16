f6630438ba6f06dc9cef4eaea9a677270554ac27
TJMusiitwa <jonamusiitwa@outlook.com>
2023-04-16 03:19:36 +0100

    Modify navigation bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -22,31 +22,18 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Veggie Android Seasons'),
-        elevation: 0,
-      ),
-      body: Container(
-        child: _pages.elementAt(_currentPage),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.book), label: 'My Garden'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.settings), label: 'Settings'),
+          centerTitle: true, title: const Text('Veggie Android Seasons')),
+      body: SizedBox(child: _pages.elementAt(_currentPage)),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.book), label: 'My Garden'),
+          NavigationDestination(icon: Icon(Icons.search), label: 'Search'),
+          NavigationDestination(icon: Icon(Icons.settings), label: 'Settings'),
         ],
-        currentIndex: _currentPage,
-        useLegacyColorScheme: false,
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        selectedItemColor: Theme.of(context).colorScheme.onPrimary,
-        unselectedItemColor: Theme.of(context).disabledColor,
-        type: BottomNavigationBarType.fixed,
-        onTap: (int index) => setState(() => _currentPage = index),
+        selectedIndex: _currentPage,
+        onDestinationSelected: (int index) =>
+            setState(() => _currentPage = index),
       ),
     );
   }
